35af5c8c1467b651497fb9fce0e914421a046ddb
Paul Schreiber <paulschreiber@gmail.com>
2011-03-08 11:13:14 -0500

    updated init.rb to work with rails 3

--- init.rb
+++ init.rb
@@ -2,4 +2,4 @@ Dir[File.join("#{File.dirname(__FILE__)}/config/locales/*.yml")].each do |locale
   I18n.load_path.unshift(locale)
 end
 
-require 'validates_word_count'
+require File.join(File.dirname(__FILE__), 'lib/validates_word_count')
